16c84de91da73f114521d9767d1d4f5730d30896
HN967 <231691448+HN967@users.noreply.github.com>
2026-07-20 03:35:30 +0000

    solve: 15 problems (2026-07-20)
    - easy-max-001
    - easy-minmax-001
    - easy-orderby-001
    - easy-select-001
    - easy-select-005
    - easy-sum-001
    - easy-avg-001
    - easy-distinct-006
    - easy-count-006
    - easy-distinct-007
    - easy-where-020
    - easy-where-023
    - easy-isnull-002
    - easy-like-001
    - easy-limit-001

---
+++ practice/sql/easy/easy-like-001/solution.sql
@@ -0,0 +1,8 @@
+-- Xom Data · Basic-plan subscribers
+-- Problem: https://xomdata.com/practice/easy-like-001
+-- Solved: 2026-07-20
+
+SELECT phone_number, full_name, plans
+FROM subscribers
+WHERE plans LIKE '%basic%'
+ORDER BY full_name ASC;
